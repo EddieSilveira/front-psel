5816ebff0a79386e2d8a5d4640f65da3c7effe17
EddieSilveira <eddie_aparecido@hotmail.com>
2021-09-12 22:25:13 -0300

    Implementadas as rotas de navegação

---
+++ src/contexts/auth.js
@@ -0,0 +1,53 @@
+import React, { createContext, useState } from 'react';
+import { BACKEND } from '../constants';
+import { useHistory } from 'react-router-dom';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [token, setToken] = useState(null);
+  const history = useHistory();
+
+  async function signIn(dataForm) {
+    const url = `${BACKEND}/signin`;
+
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        Accepts: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataForm),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.auth) {
+          localStorage.setItem('token', JSON.stringify(data.token));
+          setAuthenticated(true);
+          setToken(data.token);
+          history.push('/dashboard');
+        } else {
+          alert('Login inválido!');
+        }
+      })
+      .catch(function (error) {
+        console.error(`Houve um porblema ao fazer o login ${error.message}`);
+      });
+  }
+
+  async function signOut() {
+    setAuthenticated(false);
+    localStorage.removeItem('token');
+    setToken(null);
+    history.push('/signin');
+  }
+
+  return (
+    <AuthContext.Provider value={{ authenticated, signIn, signOut, token }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
---
+++ src/routes/Routes.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import { AuthContext } from '../contexts/auth';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+
+function CustomRoute({ isPrivate, ...rest }) {
+  const { authenticated } = useContext(AuthContext);
+
+  if (isPrivate && !authenticated) {
+    return <Redirect to="/signin" />;
+  }
+
+  return <Route {...rest} />;
+}
+
+export default function Routes() {
+  return (
+    <Switch>
+      <CustomRoute exact path="/" component={SignIn} />
+      <CustomRoute exact path="/signup" component={SignUp} />
+      <CustomRoute exact path="/dashboard" component={Dashboard} />
+    </Switch>
+  );
+}
